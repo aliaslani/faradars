fd81bce1900c753f0578bca488bc0bb3cb1eb7ae
aliaslani <cinortcele1@gmail.com>
2021-05-20 18:17:22 +0430

    setting login_manager parameters

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,6 +11,8 @@ def create_app():
     app.config['SECRET_KEY'] = 'my secret key for flask'.encode('utf8')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    
+
 
 
     from .views import views
@@ -21,6 +24,15 @@ def create_app():
     from .models import User, Note
     
     create_database(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = "برای دیدن این صفحه باید وارد پنل کاربری خود شوید"
+    login_manager.login_message_category = "error"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -14,13 +15,20 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('با موفقیت وارد حساب کاربری خود شدید', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
+            else:
+                flash('اطلاعات وارد شده صحیح نیست، لطفا دوباره تلاش کنید.', category='error')
+        else:
+            flash('کاربری با این ایمیل وجود ندارد. اگر تاکنون ثبت نام نکرده اید لطفا ثبت نام کنید.', category='error')
         
-    return render_template('login.html', title='ورود')
+    return render_template('login.html', title='ورود', user=current_user)
 
 @auth.route('/logout', methods=['POST', 'GET'])
+@login_required
 def logout():
-    return render_template('logout.html', title='خروج')
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/signup', methods=['POST', 'GET'])
 def signup():
@@ -30,6 +38,9 @@ def signup():
         email = request.form.get('email')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('این ایمیل وجود دارد. اگر قبلا ثبت نام کرده اید از طریق منوی ورود وارد سایت شوید.', category='error')
         if len(firstname) < 3:
             flash('نامی که وارد می کنید باید حداقل سه حرف باشد.', category='error')
         elif len(lastname) < 2:
@@ -47,5 +58,5 @@ def signup():
             flash('ثبت نام شما با موفقیت انجام شد.', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template('signup.html', title='ثبت نام')
+    return render_template('signup.html', title='ثبت نام', user=current_user)
 
